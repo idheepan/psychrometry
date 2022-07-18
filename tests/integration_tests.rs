2f6fdc03898fd71764072d98294e1274f5d8ea4e
Dheepan <idheepan@outlook.com>
2022-07-17 22:37:44 -0400

    Moved unit tests of temperature to temperature.rs

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,23 +1,9 @@
 /// Test functions of the library
 /// 
 
-use psychrometry::{quantities::Temperature ,units::{Celcius, Fahrenheit}};
+use psychrometry::{quantities::Temperature ,units::{Celcius, Fahrenheit, Kelvin}};
 
 mod tests {
-    use super::*;
 
-    #[test]
-    /// Simple tests. Compared with psychrolib packages
-    fn celcius_test() {
-        let a = 100.1_f64;
-        let b = 212.18_f64;
-        let t0 = Temperature::<Fahrenheit>::from(b);
-        let t1 = Temperature::<Celcius>::from(a);
-        let t2 =  Temperature::<Fahrenheit>::from(&t1);
-        // assert_eq!(t1, t2);
-
-        let t4 = t2 -3;
-        // let tr = i64::from(t2);
-        assert_eq!(t0, t4);
-    }
 }
+
